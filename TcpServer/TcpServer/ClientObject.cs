326ceab8c055e71393fac9b42c046d36ff3e42f5
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-11-22 19:04:41 +0700

    Update ClientObject.cs

--- TcpServer/TcpServer/ClientObject.cs
+++ TcpServer/TcpServer/ClientObject.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Sockets;
+using TcpServer.Services;
 
 namespace TcpServerApp
 {
@@ -12,17 +13,38 @@ namespace TcpServerApp
 
         private TcpClient _client;
         private ServerObject _server;
+        private ILogger _logger;
 
-        public ClientObject(TcpClient client, ServerObject server)
+        public ClientObject(TcpClient client, ServerObject server, ILogger logger)
         {
             _client = client;
             _server = server;
+            _logger = logger;
 
             var stream = client.GetStream();
             Reader = new(stream);
             Writer = new(stream);
         }
 
+        /// <summary>
+        /// Ассинхронная инициализация клиента клиента
+        /// </summary>
+        /// <returns></returns>
+        public async Task StartupAsync()
+        {
+             try
+            {
+                _userName = await Reader.ReadLineAsync();
+                string? message = $"{_userName} - вошел в чат!";
+
+                await SendMessageAsync(_userName, message);
+            }
+            catch (Exception ex)
+            {
+                _logger.Log($"ClientObject.StartupAsync: {ex.Message}");
+            }
+        }
+        
         /// <summary>
         /// Используется паттерн Update для цикличного прослушивания сообщений от клиента
         /// </summary>
@@ -31,21 +53,16 @@ namespace TcpServerApp
         {
             try
             {
-                _userName = await Reader.ReadLineAsync();
-                string? message = $"{_userName} - вошел в чат!";
-
-                await SendMessageAsync(_userName, message);
-
                 // В бесконечном цикле получаем сообщения от клиента
                 while (true)
                 {
                     try
                     {
                         message = await Reader.ReadLineAsync();
-
+    
                         if (message == null)
                             continue;
-
+    
                         await SendMessageAsync(_userName, message);
                     }
                     catch
@@ -58,7 +75,7 @@ namespace TcpServerApp
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"ClientObject.UpdateAsync(Exception): {ex.Message}");
+                _logger.Log($"ClientObject.UpdateAsync(Exception): {ex.Message}");
             }
             finally
             {
@@ -85,7 +102,7 @@ namespace TcpServerApp
         private async Task SendMessageAsync(string from, string message)
         {
             await _server.BroadcastSendMessageAsync(from, message, ID);
-            Console.WriteLine($"{from}: " + message);
+            _logger.Log($"{from}: " + message);
         }
 
         /// <summary>
@@ -98,7 +115,7 @@ namespace TcpServerApp
         private async Task SendMessageAsync(string from, string message, string recipientID)
         {
             await _server.SinglecastSendMessageAsync(from, message, ID, recipientID);
-            Console.WriteLine($"{from}: " + message);
+            _logger.Log($"{from}: " + message);
         }
 
         /// <summary>
@@ -111,7 +128,7 @@ namespace TcpServerApp
         private async Task SendMessageAsync(string from, string message, List<string> recipientsID)
         {
             await _server.MulticastSendMessageAsync(from, message, ID, recipientsID);
-            Console.WriteLine($"{from}: " + message);
+            _logger.Log($"{from}: " + message);
         }
     }
 
@@ -122,3 +139,4 @@ namespace TcpServerApp
         Muslticast
     }
 }
+
