0f07d5ebc994b3c576d3febf90563e5a97d734da
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-11-30 18:42:18 +0700

    Delete TcpClientApp directory

--- TcpClientApp/TcpClientApp/ConsoleHelper.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TcpClientApp
-{
-    public static class ConsoleHelper
-    {
-        public static string MSG_ForEnterMessage = "Для отправки сообщения введите сообщени и нажмите ВВОД ...";
-    }
-}
